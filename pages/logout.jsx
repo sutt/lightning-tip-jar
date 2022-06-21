89b73f2a582904ee50721cecebf58b92793e9cfc
Will Sutton <wsutton17@gmail.com>
2022-06-20 21:53:35 -0400

    fix build err

--- pages/logout.jsx
+++ pages/logout.jsx
@@ -2,6 +2,6 @@ import { useRouter } from 'next/router'
 
 export default function LogoutPage() {
   const router = useRouter()
-  router.push('/api/auth/logout')
+  // router.push('/api/auth/logout')
   return
 }
